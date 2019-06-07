ba19a73c7d4809011ac860c346353e67a4731d84
Jokubas Maciulaitis <ubitas52@gmail.com>
2019-06-07 22:22:41 +0300

    push changes

--- target/linux/hisilicon/hi3536dv100/target.mk
+++ target/linux/hisilicon/hi3536dv100/target.mk
@@ -3,11 +3,11 @@
 # Copyright (C) 2017 LEDE project
 #
 
-SUBTARGET:=hi3536d
+SUBTARGET:=hi3536dv100
 BOARDNAME:=HI3536DV100 based boards
 CPU_TYPE:=cortex-a7
 CPU_SUBTYPE:=neon-vfpv4
 
 define Target/Description
-	Build firmware image for Hisilicon HI3536DV100 SoC devices.
+	Build firmware images for Hisilicon HI3536DV100 SoC based devices.
 endef
